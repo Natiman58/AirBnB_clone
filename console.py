bb2f28aebd6e72ccea3dfaa811958a411bc00c0b
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-08-02 10:24:42 +0300

    first

--- console.py
+++ console.py
@@ -5,6 +5,8 @@
 """
 
 import cmd
+import re
+
 
 
 class ABConsole(cmd.Cmd):
@@ -12,7 +14,43 @@ class ABConsole(cmd.Cmd):
     prompt = "(hbnb) "
 
     def default(self, line):
-        """"""
+        """
+            Method called on an input line when the command prefix is not recognized.\
+            If this method is not overridden\
+            it prints an error message and returns."""
+        self.__precmd(line)
+
+    def _precmd(self, line):
+        """Executed just before the cmd line is interpreted\
+         and after the input prompt is generated"""
+        match = re.search(r"^(\w*)\.(\w+)(?:\(([^)]*)\))$", line)
+        if not match:
+            return line
+        classname = match.group(1)
+        method = match.group(2)
+        args = match.group(3)
+        match_uid_and_args = re.search('^"([^"]*)"(?:, (.*))?$', args)
+        if match_uid_and_args:
+            uid = match_uid_and_args.group(1)
+            attr_or_dict = match_uid_and_args.group(2)
+        else:
+            uid = args
+            attr_or_dict = False
+
+        attr_and_value = ""
+        if method == "update" and attr_or_dict:
+            match_dict = re.search('^({.*})$', attr_or_dict)
+            if match_dict:
+                self.update_dict(classname, uid, match_dict.group(1))
+                return ""
+            match_attr_and_value = re.search('^(?:"([^"]*)")?(?:, (.*))?$',
+                                             attr_or_dict)
+            if match_attr_and_value:
+                attr_and_value = (match_attr_and_value.group(1) or ""
+                                  + " " + (match_attr_and_value.group(2) or ""))
+        command = method + " " + classname + " " + uid + " " + attr_and_value
+        self.onecmd(command)
+        return command
 
 
 if __name__ == '__main__':
--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,41 @@
-#!/usr/bin/env python3
-"""
-    A module that defines the base class for other classes
-"""
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    """
-        The base class with public instance attributes:
-            id:
-            created_at: datetime - assign with the current datetime\
-                when an instance is created
-            updated_at: datetime - assign with the current datetime\
-                when an instance is created\
-                and it will be updated every time you change your object
-    """
-    
+    """A base class for all instances"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """To print the string form of the object"""
+        string = "[{}] () {}".format(type(self).__name__, self.id, self.__dict__)
+        print(string)
+
+    def save(self):
+        """updates the public instance attribute update_at with current time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary with key/value"""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
+
+
+
+
+
+
+
+
+
+
+
+
+
