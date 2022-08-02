06d215df057e91435775295d5e4b7069c6826132
Natiman58 <natiman58@gmail.com>
2022-08-02 15:41:40 +0300

    4

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,26 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all instances"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+            Initialize with public attributes(id, created_at, updated_at)
+            *args: list of args
+            **kwargs: key/value args
+        """
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == 'created_at':
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """To print the string form of the object"""
